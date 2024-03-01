e7502614f96156bbaa04cdecd225f073b3ed1f73
flutesandyou <flutesandyou@gmail.com>
2024-03-01 00:47:08 -0800

    [Movement] Added CustomMovementMode for Covering
    [Movement] Added CustomMovementMode for Covering
    [Movement] Added struct for parameters coming from CapsuleSweeps
    [Movement] Going to move character with PhysCustom //TODO Finish it

--- Source/Cover/Components/MovementComponents/CVBaseCharacterMovementComponent.cpp
+++ Source/Cover/Components/MovementComponents/CVBaseCharacterMovementComponent.cpp
@@ -2,4 +2,75 @@
 
 
 #include "../MovementComponents/CVBaseCharacterMovementComponent.h"
+#include "Curves/CurveVector.h"
 
+void UCVBaseCharacterMovementComponent::StartMantle(const FCoveringMovementParameters& CoveringParameters)
+{
+	CurrentCoveringParameters = CoveringParameters;
+	SetMovementMode(EMovementMode::MOVE_Custom, (uint8)ECustomMovementMode::CMOVE_Covering);
+}
+
+void UCVBaseCharacterMovementComponent::EndMantle()
+{
+	SetMovementMode(MOVE_Walking);
+}
+
+bool UCVBaseCharacterMovementComponent::IsTakingCover() const
+{
+	//return UpdatedComponent && MovementMode == MOVE_Custom && CustomMovementMode == (uint8)ECustomMovementMode::CMOVE_Covering;
+	return false;
+}
+
+void UCVBaseCharacterMovementComponent::PhysCustom(float DeltaTime, int32 Iterations)
+{
+	switch (CustomMovementMode)
+	{
+	case (uint8)ECustomMovementMode::CMOVE_Covering:
+	{
+		//float ElapsedTime = GetWorld()->GetTimerManager().GetTimerElapsed(CoveringTimer) + CurrentCoveringParameters.StartTime;
+		//FVector CoveringCurveValue = CurrentCoveringParameters.CoveringCurve->GetVectorValue(ElapsedTime);
+
+		//float PositionAlpha = CoveringCurveValue.X;
+		//float XYCorrectionAlpha = CoveringCurveValue.Y;
+		//float ZCorrectionAlpha = CoveringCurveValue.Z;
+
+		//FVector CorrectedInitialLocation = FMath::Lerp(CurrentCoveringParameters.InitialLocation, CurrentCoveringParameters.InitialAnimationLocation, XYCorrectionAlpha);
+		//CorrectedInitialLocation.Z = FMath::Lerp(CurrentCoveringParameters.InitialLocation.Z, CurrentCoveringParameters.InitialAnimationLocation.Z, ZCorrectionAlpha);
+
+		//FVector NewLocation = FMath::Lerp(CorrectedInitialLocation, CurrentCoveringParameters.TargetLocation, PositionAlpha);
+		//FRotator NewRotation = FMath::Lerp(CurrentCoveringParameters.InitialRotation, CurrentCoveringParameters.TargetRotation, PositionAlpha);
+
+		//NewLocation += CurrentCoveringParameters.HitComponent->GetComponentLocation();
+		//NewRotation += CurrentCoveringParameters.HitComponent->GetComponentRotation();
+
+		//FVector Delta = NewLocation - GetActorLocation();
+
+		//FHitResult HitResult;
+		//SafeMoveUpdatedComponent(Delta, NewRotation, false, HitResult);
+		break;
+	}
+	default:
+		break;
+	}
+
+	Super::PhysCustom(DeltaTime, Iterations);
+}
+
+void UCVBaseCharacterMovementComponent::OnMovementModeChanged(EMovementMode PreviousMovementMode, uint8 PreviousCustomMode)
+{
+	Super::OnMovementModeChanged(PreviousMovementMode, PreviousCustomMode);
+
+	if (MovementMode == MOVE_Custom)
+	{
+		//switch (CustomMovementMode)
+		//{
+		//case (uint8)ECustomMovementMode::CMOVE_Covering:
+		//{
+		//	GetWorld()->GetTimerManager().SetTimer(CoveringTimer, this, &UCVBaseCharacterMovementComponent::EndTakingCover, CurrentCoveringParameters.Duration, false);
+		//	break;
+		//}
+		//default:
+		//	break;
+		//}
+	}
+}
--- Source/Cover/Components/MovementComponents/CVBaseCharacterMovementComponent.h
+++ Source/Cover/Components/MovementComponents/CVBaseCharacterMovementComponent.h
@@ -6,12 +6,44 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "CVBaseCharacterMovementComponent.generated.h"
 
-/**
- * 
- */
+struct FCoveringMovementParameters
+{
+	FVector InitialLocation = FVector::ZeroVector;
+	FRotator InitialRotation = FRotator::ZeroRotator;
+	UPrimitiveComponent* HitComponent = nullptr;
+	FVector TargetLocation = FVector::ZeroVector;
+	FRotator TargetRotation = FRotator::ZeroRotator;
+
+	FVector InitialAnimationLocation = FVector::ZeroVector;
+
+	float Duration = 1.0f;
+	float StartTime = 0.0f;
+
+	UCurveVector* CoveringCurve;
+};
+
+UENUM(BlueprintType)
+enum class ECustomMovementMode : uint8
+{
+	CMOVE_None = 0 UMETA(DisplayName = "None"),
+	CMOVE_Covering UMETA(DisplayName = "Covering"),
+	CMOVE_Max UMETA(Hidden)
+};
+
 UCLASS()
 class COVER_API UCVBaseCharacterMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	void StartMantle(const FCoveringMovementParameters& CoveringParameters);
+	void EndMantle();
+	bool IsTakingCover() const;
+
+protected:
+	virtual void PhysCustom(float DeltaTime, int32 Iterations) override;
+	virtual void OnMovementModeChanged(EMovementMode PreviousMovementMode, uint8 PreviousCustomMode) override;
+
+private:
+	FCoveringMovementParameters CurrentCoveringParameters;
 };
